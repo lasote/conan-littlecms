c1348e16c1fd4ecf5e8dcd635df0cbbe36d456fd
Lasote <lasote@gmail.com>
2016-03-03 16:26:37 +0100

    Remove shared on windows

--- build.py
+++ build.py
@@ -4,4 +4,13 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="littlecms:shared", pure_c=True)
+    if platform.system() == "Windows":
+        # Remove shared builds in windows
+        static_builds = []
+        for build in builder.builds:
+            if not build[1]["littlecms:shared"]:
+                static_builds.append([build[0], {}])
+            
+        builder.builds = static_builds   
+
     builder.run()
